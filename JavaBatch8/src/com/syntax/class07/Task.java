97649d5d7a0d9bbaca1615bac9907ed62aff2e9e
EliasAlvial <Elias.Alvial@gmail.com>
2020-10-17 13:41:50 -0400

    While loop demo

---
+++ JavaBatch8/src/com/syntax/class07/Task.java
@@ -0,0 +1,19 @@
+package com.syntax.class07;
+
+public class Task {
+public static void main(String[] args) {
+	boolean workday=true;
+			int day=1;
+			while(workday){	
+		if(day<=5) {
+				System.out.println("I need a day off");
+		
+		}else{
+			System.out.println("I do not need a day off anymore");
+			workday=false;
+		}
+		day++;
+			}
+			
+}
+}
