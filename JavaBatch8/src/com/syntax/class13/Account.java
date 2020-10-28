2a401b475679e59a16e446f90db0b6882c5d8cc0
EliasAlvial <Elias.Alvial@gmail.com>
2020-10-27 22:25:34 -0400

    10-27

---
+++ JavaBatch8/src/com/syntax/class13/Account.java
@@ -0,0 +1,35 @@
+package com.syntax.class13;
+
+public class Account {
+    String accountId;
+    String userName;
+    String password;
+    double balance;
+    String typeOfAccount;
+    double limit;
+    boolean isBlocked;
+   //write a method that takes the amnount that user wantes to withdraw
+    //transfer it deducgts the amount from your balance
+    //returns the amount of transfer in case of succes or a 0
+    //if false falure
+
+    boolean login(String enteredUserName, String enteredpassword) {//speccify datatype, first
+        if (userName.equals(enteredUserName) &&
+                password.equals(enteredpassword)) {
+            return true;//this is consistant with datatype
+        } else {
+            return false;
+        }
+    }
+    double transfer(double transferAmount){
+        if(limit>=transferAmount){
+           return balance=limit-transferAmount;
+        }else {
+            return 0;
+        }
+
+    }
+
+
+
+}
---
+++ JavaBatch8/src/com/syntax/class13/Main.java
@@ -0,0 +1,20 @@
+package com.syntax.class13;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1=new Dog();
+        dog1.name="Sophie";
+        dog1.breed="Husky";
+        dog1.color="White";
+        dog1.loyalty=true;
+        dog1.age=4;
+        dog1.gender="Female";
+        dog1.getsBeer();
+        dog1.bark();
+        dog1.play();
+
+
+
+
+    }
+}
